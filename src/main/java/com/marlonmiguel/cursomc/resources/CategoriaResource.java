fab24c2263956cd2942d29d8d24aa1bd727b827f
MarlonMiguel <marlonmiguel1993@gmail.com>
2023-10-12 19:02:57 -0300

    Criação automatica de duas categorias

--- src/main/java/com/marlonmiguel/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marlonmiguel/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,4 +26,5 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(obj);
 	}
+
 }
